d2da8ccf2f33ac796892963aca1ef9df073804b6
IsaacNudeton <isaacboy722@gmail.com>
2025-11-10 00:52:50 -0800

    Add files via upload

---
+++ Views/PerformanceAnalyticsView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace PCOptimizer.Views
+{
+    public partial class PerformanceAnalyticsView : UserControl
+    {
+        public PerformanceAnalyticsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Views/SettingsView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace PCOptimizer.Views
+{
+    public partial class SettingsView : UserControl
+    {
+        public SettingsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
